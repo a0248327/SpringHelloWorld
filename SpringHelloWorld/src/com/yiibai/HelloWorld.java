23ad5e7b4b95d099f02939ff6e3f6f0090fab30d
Han Dian <muxin_email@hotmail.com>
2015-01-18 18:51:12 +0100

    002

--- SpringHelloWorld/src/com/yiibai/HelloWorld.java
+++ SpringHelloWorld/src/com/yiibai/HelloWorld.java
@@ -1,14 +1,14 @@
 package com.yiibai;
 
 public class HelloWorld {
-	
-   private String message;
 
-   public void setMessage(String message){
-      this.message  = message;
-   }
+	private String message;
 
-   public void getMessage(){
-      System.out.println("Your Message : " + message);
-   }
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void getMessage() {
+		System.out.println("Your Message : " + message);
+	}
 }
